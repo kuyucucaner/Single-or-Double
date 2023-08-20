a4d4e5202445caff5a0e225c9dd44f08e72a48d2
kuyucucaner <141584156+kuyucucaner@users.noreply.github.com>
2023-08-20 16:33:01 +0300

    Add files via upload

---
+++ TekmiCiftmi/TekmiCiftmi/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TekmiCiftmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            if (sayi % 2 == 0)
+                MessageBox.Show("Girilen Sayı Bir ÇİFT Sayıdır!");
+            else
+                MessageBox.Show("Girilen Sayı Bir TEK Sayıdır!");
+        }
+    }
+}
